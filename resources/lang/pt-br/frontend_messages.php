993b9a65972a8e95ad3c1bfef9d319cfd150ca3d
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-10-10 13:25:47 -0300

    feat(traducao): Traduzida todas as mensagens nas páginas de login e registro

---
+++ resources/lang/pt-br/frontend_messages.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafael
+ * Date: 10/10/18
+ * Time: 13:13
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Frontend messages
+    |--------------------------------------------------------------------------
+    |
+    | All messages that will be used in frontend.
+    |
+    */
+
+    // Login and Register page
+    'register'      => 'Registro',
+    'email_address' => 'E-mail',
+    'name'          => 'Nome',
+    'password'      => 'Senha',
+    'confirm_password' => 'Confirme a senha',
+    'remember_me'   => 'Lembre de mim',
+    'forgot_your_password'  => 'Esqueceu sua senha?',
+
+    // Generals
+    'send'  => 'Enviar',
+
+];
