26571ee32f7f739483d931010475711aee6f2d5f
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-10-10 12:54:04 -0300

    feat(app): Adicionado favicon e setado nome do app para LaraCTF

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
+
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
