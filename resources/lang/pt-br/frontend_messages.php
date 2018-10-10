ccb7a39c473e2fb8493b061e77aeb0573636a6ac
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-10-10 13:52:21 -0300

    feat(traducao): Add tradução em páginas de verificar email e redefinir senha

--- resources/lang/pt-br/frontend_messages.php
+++ resources/lang/pt-br/frontend_messages.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    // Login and Register page
+    // Auth pages
     'register'      => 'Registro',
     'email_address' => 'E-mail',
     'name'          => 'Nome',
@@ -25,6 +25,13 @@ return [
     'confirm_password' => 'Confirme a senha',
     'remember_me'   => 'Lembre de mim',
     'forgot_your_password'  => 'Esqueceu sua senha?',
+    'reset_password'    => 'Redefinir senha',
+    'send_password_link_reset'  => 'Enviar link de redefinição de senha',
+    'verify_your_email_address' => 'Verifique seu endereço de e-mail',
+    'new_verification_link_has_been_sent'   => 'Um novo link de verificação foi enviado para seu endereço de e-mail.',
+    'before_check_your_email'   => 'Antes de prosseguir, verifique seu e-mail em busca de um link de verificação.',
+    'if_you_did_not_receive'  => 'Se você não recebeu o email',
+    'click_here_to_request_another' => 'clique aqui para solicitar outro',
 
     // Others
     'send'  => 'Enviar',
